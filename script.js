59dcda24e9a1c5e7f52f809df1e9db12c572a884
J-E-Y <rightfuljung@gmail.com>
2019-09-07 16:50:29 +0900

    이름 날짜 댓글내용 화면에 보이기

--- script.js
+++ script.js
@@ -1,9 +1,42 @@
-// your code here
 
-// DATA는 이미 작성된 트윗을 표시합니다.
-console.log(DATA)
 
-// generateNewTweet을 호출할 때마다 새로운 트윗을 생성합니다.
-console.log(generateNewTweet());
+//  1. 이름 날짜 댓글내용 출력하기 
 
-document.getElementById('test').innerHTML = 'hello twittler, check developer console!';
+function printTweet(){
+
+    for(let i=0; i<5;i++){
+        
+        // <ul class = "list">  element 생성해서 comments 안에 넣는다. 
+
+        let div = document.createElement("div")
+        div.setAttribute("class", "comment")
+        document.querySelector("#comments").appendChild(div)
+
+        // <li class = "user-comment"> element 생성해서 ul 안에 얺는다
+
+        let id = document.createElement("span");
+        id.setAttribute("class","user-comment");
+        id.innerText = randomUser[getRandomInt(0, randomUser.length)];
+        document.querySelectorAll(".comment")[i].appendChild(id);
+
+        // <li class = "time-comment"> element 생성해서 ul 안에 얺는다
+        
+        let time = document.createElement("span");
+        time.setAttribute("class","time-comment");
+        time.innerText = new Date().format();
+        document.querySelectorAll(".comment")[i].appendChild(time);
+
+        // <li class = "message-comment"> element 생성해서 ul 안에 얺는다
+
+        let message = document.createElement("span");
+        message.setAttribute("class", "message-comment");
+        message.innerText = randomMessage[getRandomInt(0, randomMessage.length)];
+        document.querySelectorAll(".comment")[i].appendChild(message);
+
+
+    }  
+}
+printTweet();
+
+
+// refresh 버튼을 누르면 새로고침  
